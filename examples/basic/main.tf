3f4c9937d681a77c761a91e0a9c5ea6b7be22a04
Chiara Holzgräfe <chiara.holzgraefe@qbeyond.de>
2023-10-27 16:09:27 +0200

    terraform files

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,14 @@
 provider "azurerm" {
-  features {
+  features {}
+}
 
-  }
+module "vse_subscription_mover" {
+  source = "../.."
+  location = "westeurope"
+  resource_group_name = "rg-SubscriptionMover-dev-01"
+  function_name = "func-dev-SubscriptionMover-chhol-01"
+  app_service_plan_name = "plan-dev-SubscriptionMover-chhol-01"
+  
+  storage_account_name = "stfunchholsubsmover01"
+
 }
--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                      = var.storage_account_name
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  //allow_blob_public_access  = false
+  enable_https_traffic_only = true
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  os_type = "Windows"
+  sku_name = "Y1"
+}
+
+
+
+resource "azurerm_windows_function_app" "function" {
+  name                       = var.function_name
+  resource_group_name        = azurerm_resource_group.example.name
+  location                   = var.location
+  service_plan_id        = azurerm_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  //version                    = "~3"
+
+  site_config {}
+  
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"       = "powershell"
+    "FUNCTIONS_WORKER_RUNTIME_VERSION" = "~7"
+  }
+  
+}
